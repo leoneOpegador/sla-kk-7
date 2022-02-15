f5341e3a95371567b61bcd89b9ae5d089c34f239
leoneOpegador <92598924+leoneOpegador@users.noreply.github.com>
2022-02-15 17:20:24 -0300

    Add files via upload

---
+++ MatterJSBoilerPlate-mainpt-main/sketch.js
@@ -0,0 +1,91 @@
+
+const Engine = Matter.Engine;
+const World = Matter.World;
+const Bodies = Matter.Bodies;
+const Body = Matter.Body;
+
+var block, block1, block2
+
+function preload()
+{
+	
+}
+
+function setup() {
+	createCanvas(800, 700);
+	
+	engine = Engine.create();
+	world = engine.world;
+
+	Engine.run(engine);
+
+
+	var plane_options={
+		isStatic:true
+	}
+
+
+	plane = Bodies.rectangle(600,580,1200,2,plane_options)
+	World.add(world,plane)
+	
+	var block_options = {
+		restitution:0.5,
+		friction:0.02,
+		frictionAir:0
+	}
+
+	var block1_options = {
+		restitution:0.7,
+		friction:0.01,
+		frictionAir:0.1
+	}
+
+	var block2_options = {
+		restitution:0.01,
+		friction:1,
+		frictionAir:0.3
+	}
+
+
+	//Crie os Corpos Aqui.
+	block = Bodies.circle(220,10,10,block_options);
+	World.add(world,block)
+
+	block1 = Bodies.rectangle(110,50,10,10,block1_options);
+	World.add(world,block1)
+
+	block2 = Bodies.rectangle(350,50,10,10,block2_options);
+	World.add(world,block2)
+
+
+	
+  
+
+
+}
+
+
+function draw() {
+  rectMode(CENTER);
+  background(0);
+  
+	
+  Engine.update(engine);
+
+rect(plane.position.x,plane.position.y,1200)
+
+ellipse(block.position.x,block.position.y,30);
+rect(block1.position.x,block1.position.y,50,50);
+rect(block2.position.x,block2.position.y,100,50)
+
+
+  drawSprites();
+ 
+
+
+
+
+}
+
+
+
